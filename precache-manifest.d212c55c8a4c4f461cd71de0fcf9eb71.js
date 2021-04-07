5b7e00f6c3f0f680b988b3716e32497102a7d497
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-04-07 23:02:26 +0530

    Updates

---
+++ precache-manifest.d212c55c8a4c4f461cd71de0fcf9eb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351229ba86f458d36d5eda17d450b657",
+    "url": "/profiles/index.html"
+  },
+  {
+    "revision": "365ecc40ad13928346ea",
+    "url": "/profiles/static/css/main.3b95fe33.chunk.css"
+  },
+  {
+    "revision": "d99b5deab8163667b493",
+    "url": "/profiles/static/js/2.baf15b84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profiles/static/js/2.baf15b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365ecc40ad13928346ea",
+    "url": "/profiles/static/js/main.8b722fe4.chunk.js"
+  },
+  {
+    "revision": "743b2acce83e8eb77053",
+    "url": "/profiles/static/js/runtime-main.4b0f733f.js"
+  }
+]);
